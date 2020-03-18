74612fd549b680435d7866599bf707260fbfbca2
himanshu7 <34533668+himanshukholiya@users.noreply.github.com>
2020-03-18 15:55:47 +0530

    Update skills.py

--- skills.py
+++ skills.py
@@ -1,22 +1,37 @@
 import csv
+import re
+
 def extract_skills(resume_string):
-    # resume_string1 = resume_string
     resume_string = resume_string.replace(',',' ')
+
     #Converting all the charachters in lower case
-    resume_string = resume_string.lower()
+    
+    resume_string= resume_string.lower()
+    # resume_string = re.finditer(r'[a-zA-Z0-9 \.\/\++\@\(\)\]', resume_string)
 
-    with open(r"C:\Users\HimanshuKholiya\Desktop\UI\skill.csv", 'rt') as f:
+    with open(r"C:\Users\HimanshuKholiya\Desktop\newUI\programmingskills.csv", 'rt') as f:
         reader = csv.reader(f)
-        your_list = list(reader)
-        your_list = your_list[0]
-        your_list = [word.lower() for word in your_list]
+        your_list1 = list(reader)
+        your_list1 = set(your_list1[0])
+        your_list1 = [word.lower() for word in your_list1]
 
-    #Sets are used as it has a a constant time for lookup hence the overall the time for the total code will not exceed O(n)
-    s = set(your_list)
+    with open(r"C:\Users\HimanshuKholiya\Desktop\newUI\toolandsoftware.csv", 'rt') as f:
+        reader = csv.reader(f)
+        your_list2 = list(reader)
+        your_list2 = set(your_list2[0])
+        your_list2 = [word.lower() for word in your_list2] 
 
-    list_matched = []
-    for item in your_list:
-        if item in resume_string:
-            list_matched.append(item)
+    your_list = your_list1+your_list2
 
-    return list_matched
+    your_list = ' | '.join(your_list)
+    your_list = your_list.replace(r"+",r"\+").replace(r".",r"\.").replace(r')',r"").replace(r'(',r"")
+    mylist = []
+    try:
+        matched_list = re.finditer(your_list, resume_string)
+        for match in matched_list:
+            mylist.append(match.group())
+        return list(set(mylist))
+    except Exception as e:
+        print(e)
+
+

46cbbb9b25978958d9f1e96d9d2ed3f2342b54ed
himanshu7 <34533668+himanshukholiya@users.noreply.github.com>
2020-03-18 15:57:43 +0530

    Update extract_email.py

--- extract_email.py
+++ extract_email.py
@@ -1,6 +1,12 @@
 import re
-def extract_email_addresses(string):
-    r = re.compile(r'[a-zA-Z0-9_\.+%]+@[a-zA-Z0-9_\.]*\.(com|edu|net|in|org)')    
-    e_mail = r.finditer(string)
+def extract_email_addresses(text):
+    r = re.compile(r'[a-zA-Z0-9_\.]+@[a-zA-Z-\.]*\.(com|edu|net|in|org|COM|EDU|NET|IN|ORG)')
+    e_mail = r.finditer(text)
     for x in e_mail:
         return x.group(0)
+
+# def extract_email_address(fulltext):
+#     r = re.compile(r'[a-zA-Z0-9_\.]+@[a-zA-Z-\.]*\.(com|edu|net|in|org)')
+#     e_mail = r.finditer(fulltext)
+#     for x in e_mail:
+#         return x.group(0)

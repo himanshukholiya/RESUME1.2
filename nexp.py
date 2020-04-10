13922e72159c1dc3098b4ed229ca14a43a9a334a
KailashChandra-dotcom <60435678+KailashChandra-dotcom@users.noreply.github.com>
2020-04-10 09:00:10 +0530

    Update nexp.py

--- nexp.py
+++ nexp.py
@@ -7,8 +7,6 @@ from nltk.tokenize import word_tokenize
 # from datetime import datetime 
 # from dateutil.relativedelta import relativedelta
 
-
-
 experience2=[]
 
 def extract_experience(text):
@@ -16,10 +14,10 @@ def extract_experience(text):
     resume_string = ''.join(resume_string)
     indexlist = []
     found = False
-
-    lines = [el.strip() for el in resume_string.split("\n") if len(el) > 0]      # Splitting on the basis of newlines 
+    lines = [el.strip() for el in resume_string.split("\n") if len(el) > 0] # Splitting on the basis of newlines 
     lines = [sent_tokenize(el) for el in lines]                         # Tokenize the individual lines
     lines = lines[0]
+
 
     for line in lines:
         if 'experience' in line or 'exposure' in line:
@@ -29,13 +27,21 @@ def extract_experience(text):
 
     if found == True:
         experience = extract_year(indexlist, lines)
-        print(experience)
-        maxval = max(experience)
-        return maxval
-    
+        maxval = sorted(experience, key = lambda item: (int(item.partition(' ')[0]) if item[0].isdigit() else float('inf'),item));
+        if maxval[-1]!= ' year':
+            return maxval[-1]
+        else:
+            return maxval[-2]
     else:
         return 0
+    
 
+
+    # elif found == False and flag ==1:
+    #     experience_2 = extract_yeardate(lines)
+    #     print(experience_2)
+    #     flag = 0
+    #     return 0
 
     # if all!=[]:
     # all.sort(key = lambda date: datetime.strptime(date, '%b %Y'))
@@ -55,10 +61,10 @@ def extract_year(selectline, mystringarr):
     return experience1
     
 
-def extract_yeardate(myline):
-        all = re.findall(r"(?:jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)( |')(d{2,4}( to | - |-)(?:(till date)?|jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)( |')\d{2,4}", myline)
-        for item in all:
-            experience2.append(item.group())
-        
+# def extract_yeardate(myline):
+#         all = re.findall(r"(?:jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)( |')d{2,4}( to | - |-)(?:till date|jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)( |')\d{2,4}", myline)
+#         for item in all:
+#             experience2.append(item.group())
+#         return experience2
 
 
